e854267dfa8c0bec300bb26d3e8ce10a104bb842
package <wesleyamancio@Laptop-de-Wesley.local>
2025-06-21 18:26:05 -0300

    commit

--- react-template/src/App.jsx
+++ react-template/src/App.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AdminLogin from './AdminLogin';
 
 export default function App() {
+  const [isLogged, setIsLogged] = useState(false);
+
+  if (!isLogged) {
+    return <AdminLogin onLogin={() => setIsLogged(true)} />;
+  }
+
   return (
     <div className="container my-5">
       <h1 className="mb-3">SmartAdmin React Environment</h1>
-      <p className="lead">This React setup uses the SmartAdmin template assets.</p>
+      <p className="lead">Bem-vindo, administrador!</p>
     </div>
   );
 }
--- react-template/src/main.jsx
+++ react-template/src/main.jsx
@@ -1,3 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../dist/css/smartapp.css';
+import '../../dist/css/authentication.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
