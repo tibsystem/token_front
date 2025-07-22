5704aeab24eeaaf78a62fb1238101af914ae5867
Inaciosant <inaciosantana231@gmail.com>
2025-07-22 12:10:36 -0300

    Refactor: Remove unused DarkModeSwitcher CSS and update related imports

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,72 +1,93 @@
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-'use client';
+"use client";
 
-import Link from 'next/link';
-import DropdownNotification from './dropdown/notification';
-import DropdownLanguage from './dropdown/language';
-import DropdownProfile from './dropdown/profile';
-import SearchForm from './search/form';
-import DropdownMegaMenu from './dropdown/mega';
-import DarkModeSwitcher from './DarkmodeSwitcher/DarkModeSwitcher';
-import { useAppSettings } from '@/config/app-settings';
-import UseProfile from '@/hooks/UseProfile';
-import useDarkMode from '@/hooks/useDarkMode';
+import Link from "next/link";
+import DropdownNotification from "./dropdown/notification";
+import DropdownLanguage from "./dropdown/language";
+import DropdownProfile from "./dropdown/profile";
+import SearchForm from "./search/form";
+import DropdownMegaMenu from "./dropdown/mega";
+import DarkModeSwitcher from "./DarkmodeSwitcher/DarkModeSwitcher";
+import { useAppSettings } from "@/config/app-settings";
+import UseProfile from "@/hooks/UseProfile";
+import useDarkMode from "@/hooks/useDarkMode";
 
 export default function Header() {
   const { settings, updateSettings } = useAppSettings();
-  const { isDarkMode } = useDarkMode();
+  const { isDarkMode, setIsDarkMode } = useDarkMode();
 
-  
   const toggleAppSidebarEnd = (e: React.MouseEvent) => {
-  	e.preventDefault();
-  	
-  	updateSettings({
-			appSidebarEndToggled: !settings.appSidebarEndToggled
-		});
+    e.preventDefault();
+
+    updateSettings({
+      appSidebarEndToggled: !settings.appSidebarEndToggled,
+    });
   };
-  
+
   const toggleAppSidebarEndMobile = (e: React.MouseEvent) => {
-  	e.preventDefault();
-  	
-  	updateSettings({
-			appSidebarEndMobileToggled: true
-		});
+    e.preventDefault();
+
+    updateSettings({
+      appSidebarEndMobileToggled: true,
+    });
   };
-  
+
   const toggleAppSidebarMobile = () => {
-  	updateSettings({
-			appSidebarMobileToggled: true
-		});
+    updateSettings({
+      appSidebarMobileToggled: true,
+    });
   };
-  
+
   const toggleAppTopMenuMobile = () => {
-  	updateSettings({
-			appTopMenuMobileToggled: true
-		});
+    updateSettings({
+      appTopMenuMobileToggled: true,
+    });
   };
-  
-  
+
+  const setAppMode = (mode: string) => {
+    document.documentElement.setAttribute("data-bs-theme", mode);
+    localStorage.appMode = mode;
+    document.dispatchEvent(new Event("theme-reload"));
+  };
+
   return (
-    <div id="header" className="app-header" data-bs-theme={settings.appHeaderInverse ? 'dark' : ''}>
+    <div
+      id="header"
+      className="app-header"
+      data-bs-theme={settings.appHeaderInverse ? "dark" : ""}
+    >
       <div className="navbar-header">
         {settings.appSidebarTwo && (
-          <button type="button" className="navbar-mobile-toggler" onClick={toggleAppSidebarEndMobile}>
+          <button
+            type="button"
+            className="navbar-mobile-toggler"
+            onClick={toggleAppSidebarEndMobile}
+          >
             <span className="icon-bar"></span>
             <span className="icon-bar"></span>
             <span className="icon-bar"></span>
           </button>
         )}
         <Link href="#" className="navbar-brand">
-          <img 
-            src={isDarkMode ? "../../assets/img/logo-light.webp" : "../../assets/img/logo-dark.webp"} 
-            alt="Logo" 
-            className="logo" 
+          <img
+            src={
+              isDarkMode
+                ? "../../assets/img/logo-light.webp"
+                : "../../assets/img/logo-dark.webp"
+            }
+            alt="Logo"
+            className="logo"
           />
         </Link>
 
         {settings.appHeaderMegaMenu && (
-          <button type="button" className="navbar-mobile-toggler" data-bs-toggle="collapse" data-bs-target="#top-navbar">
+          <button
+            type="button"
+            className="navbar-mobile-toggler"
+            data-bs-toggle="collapse"
+            data-bs-target="#top-navbar"
+          >
             <span className="fa-stack fa-lg text-inverse">
               <i className="far fa-square fa-stack-2x"></i>
               <i className="fa fa-cog fa-stack-1x"></i>
@@ -74,7 +95,11 @@ export default function Header() {
           </button>
         )}
         {settings.appTopMenu && !settings.appSidebarNone && (
-          <button type="button" className="navbar-mobile-toggler" onClick={toggleAppTopMenuMobile}>
+          <button
+            type="button"
+            className="navbar-mobile-toggler"
+            onClick={toggleAppTopMenuMobile}
+          >
             <span className="fa-stack fa-lg text-inverse">
               <i className="far fa-square fa-stack-2x"></i>
               <i className="fa fa-cog fa-stack-1x"></i>
@@ -82,14 +107,22 @@ export default function Header() {
           </button>
         )}
         {settings.appSidebarNone && settings.appTopMenu && (
-          <button type="button" className="navbar-mobile-toggler" onClick={toggleAppTopMenuMobile}>
+          <button
+            type="button"
+            className="navbar-mobile-toggler"
+            onClick={toggleAppTopMenuMobile}
+          >
             <span className="icon-bar"></span>
             <span className="icon-bar"></span>
             <span className="icon-bar"></span>
           </button>
         )}
         {!settings.appSidebarNone && (
-          <button type="button" className="navbar-mobile-toggler" onClick={toggleAppSidebarMobile}>
+          <button
+            type="button"
+            className="navbar-mobile-toggler"
+            onClick={toggleAppSidebarMobile}
+          >
             <span className="icon-bar"></span>
             <span className="icon-bar"></span>
             <span className="icon-bar"></span>
@@ -104,15 +137,27 @@ export default function Header() {
         {/* <DropdownNotification /> */}
 
         {settings.appHeaderLanguageBar && <DropdownLanguage />}
-        <DarkModeSwitcher />
+        <DarkModeSwitcher
+          checked={isDarkMode}
+          onChange={() => {
+            setIsDarkMode(!isDarkMode);
+            setAppMode(!isDarkMode ? "dark" : "light");
+          }}
+        />
 
         <DropdownProfile />
 
-        {settings.appSidebarTwo && <div className="navbar-divider d-none d-md-block"></div>}
+        {settings.appSidebarTwo && (
+          <div className="navbar-divider d-none d-md-block"></div>
+        )}
 
         {settings.appSidebarTwo && (
           <div className="navbar-item d-none d-md-block">
-            <Link href="/" onClick={toggleAppSidebarEnd} className="navbar-link icon">
+            <Link
+              href="/"
+              onClick={toggleAppSidebarEnd}
+              className="navbar-link icon"
+            >
               <i className="fa fa-th"></i>
             </Link>
           </div>
